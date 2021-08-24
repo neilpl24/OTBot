597321fffbe4c6620c9e1da0729db85c0689d1ce
Neil <neilpl24@live.unc.edu>
2021-08-24 15:11:42 -0400

    Added a map for the new streaking feature that will be implemented

---
+++ src/streakmap.js
@@ -0,0 +1,15 @@
+// Maps all possible streak values and its multipliers.
+const streakmap = new Map();
+streakmap.set(0, 1);
+streakmap.set(1, 1);
+streakmap.set(2, 1);
+streakmap.set(3, 1.2);
+streakmap.set(4, 1.4);
+streakmap.set(5, 1.6);
+streakmap.set(6, 1.8);
+streakmap.set(7, 2.0);
+for(let i=8; i<100; i++) {
+    streakmap.set(i, 2.0);
+}
+
+module.exports = streakmap;
