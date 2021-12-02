aa2dfe3e11845b506db304e134d5944ec599eec6
Neil <neilpl24@live.unc.edu>
2021-12-02 00:31:26 -0500

    changed streaks

--- src/streakmap.js
+++ src/streakmap.js
@@ -3,13 +3,13 @@ const streakmap = new Map();
 streakmap.set(0, 1);
 streakmap.set(1, 1);
 streakmap.set(2, 1);
-streakmap.set(3, 1.2);
-streakmap.set(4, 1.4);
-streakmap.set(5, 1.6);
-streakmap.set(6, 1.8);
-streakmap.set(7, 2.0);
+streakmap.set(3, 2);
+streakmap.set(4, 4);
+streakmap.set(5, 6);
+streakmap.set(6, 8);
+streakmap.set(7, 10);
 for (let i = 8; i < 100; i++) {
-    streakmap.set(i, 2.0);
+    streakmap.set(i, 12);
 }
 
 module.exports = streakmap;
