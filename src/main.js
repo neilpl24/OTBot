8b0935e57d34ebc22ce37e369317cae39886b739
Neil <neilpl24@live.unc.edu>
2022-05-03 21:45:43 -0400

    change

--- src/main.js
+++ src/main.js
@@ -81,7 +81,7 @@ async function getSchedule() {
                 const awayTeam = gameDataArray[i].liveData.linescore.teams.away;
                 home = homeTeam.team.name;
                 away = awayTeam.team.name;
-                channel.send(`The ${homeTeam.team.name} take on the ${awayTeam.team.name} in overtime! Who is your pick? You have 10 minutes! React with the emotes below. @everyone`);
+                channel.send(`The ${homeTeam.team.name} take on  the ${awayTeam.team.name} in overtime! Who is your pick? You have 10 minutes! React with the emotes below. @everyone`);
                 // Fetches the reactions from the OT games after 5 minutes.
                 setTimeout(async () => {
                     const messages = await channel.messages.fetch({ limit: 4 });
