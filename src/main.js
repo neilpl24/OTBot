7f85a94d509a678b54b8d875bd151827d712acab
Neil Pierre-Louis <59701044+neilpl24@users.noreply.github.com>
2024-01-03 23:51:13 -0500

    this api sucks

--- src/main.js
+++ src/main.js
@@ -77,12 +77,11 @@ async function getSchedule() {
     const channel = bot.channels.cache.get("819792691511558184");
     // Determines if a game is in overtime or not.
     if (
-      gameDataArray[i].period == 3 &&
+      gameDataArray[i].periodType == "OT" &&
       gameDataArray[i].gameState == "CRIT" &&
       (gameDataArray[i].gameState != "OFF" ||
         gameDataArray[i].gameState != "FINAL" ||
         gameDataArray[i].gameState != "OVER") &&
-      gameDataArray[i].clock.inIntermission &&
       gameDataArray[i].homeTeam.score == gameDataArray.awayTeam.score
     ) {
       // Prevents the bot from sending messages multiple times about overtime.
